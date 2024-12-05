f08b0720d51cf3c167ce0d0b40f1b1847e438171
InvisOn <InvisOn@users.noreply.github.com>
2024-12-05 14:22:34 +1300

    chore:Change appId to `com.grocery_comparison.app`

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'grocery_comparison',
-  webDir: 'dist'
+    appId: 'com.grocery_comparison.app',
+    appName: 'grocery_comparison',
+    webDir: 'dist'
 };
 
 export default config;
