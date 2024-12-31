fb5c7b85d1fd5d23d1cae7dc9ddb28f2a80abdc9
InvisOn <InvisOn@users.noreply.github.com>
2024-12-31 13:13:04 +1300

    fix: fix docker compose connection

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,15 +1,13 @@
 // server connection
-export const API_URL = import.meta.env.VITE_EXPRESS_API_URL || "http://localhost:3000" || 'http://fullstack_server:3000';
+export const API_URL = import.meta.env.VITE_EXPRESS_API_URL
 // ;
 
 export const API_ENDPOINTS = {
 
-    WELCOME_API : "/",
+    WELCOME_API: "/",
     // Initital Setup Endpoints from server
-    GET_DATA: "/initialSetup", 
-    POST_DATA: "/initialSetup", 
-    PUT_DATA: "/initialSetup/:id", 
-    DELETE_DATA: "/initialSetup/:id", 
-
-    // Add more endpoints as needed
-  };
+    GET_DATA: "/initialSetup",
+    POST_DATA: "/initialSetup",
+    PUT_DATA: "/initialSetup/:id",
+    DELETE_DATA: "/initialSetup/:id",
+};
--- server/src/middlewares/corsMiddleware.ts
+++ server/src/middlewares/corsMiddleware.ts
@@ -2,18 +2,18 @@ import cors from 'cors';
 import { Application } from 'express';
 
 const allowedOrigins = [
-  'capacitor://localhost',
-  'ionic://localhost',
-  'http://localhost',
-  'http://localhost:5173',
-  'http://localhost:8100',
-  '*',
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:5173',
+    'http://localhost:8100',
+    '*',
 ];
 
 const corsMiddleware = (app: Application) => {
-  app.use(cors({
-    origin: allowedOrigins,
-  }));
+    app.use(cors({
+        // origin: allowedOrigins,
+    }));
 };
 
 export default corsMiddleware;
