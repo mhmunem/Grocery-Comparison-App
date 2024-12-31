ef373b91de20203f6826362c243ecc9d74296af0
InvisOn <InvisOn@users.noreply.github.com>
2025-01-01 10:28:11 +1300

    chore: rename `database/` to `db/`

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,22 +5,22 @@ import corsMiddleware from './middlewares/corsMiddleware';
 import bodyParserMiddleware from './middlewares/bodyParserMiddleware';
 import errorHandler from './middlewares/errorHandler';
 import routes from './routes/initialsetup';
-import db from '../database/connection/pool';
+import db from '../db/connection/pool';
 import productsRouter from './api/products';
 
 const app = express();
 
 // Swagger definition
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Grocery Comparison API',
-      version: '1.0.0',
-      description: 'API documentation for the Grocery Comparison server',
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Grocery Comparison API',
+            version: '1.0.0',
+            description: 'API documentation for the Grocery Comparison server',
+        },
     },
-  },
-  apis: ['./src/routes/initialsetup.ts'], 
+    apis: ['./src/routes/initialsetup.ts'],
 };
 
 const specs = swaggerJsdoc(options);
@@ -31,8 +31,8 @@ bodyParserMiddleware(app);
 
 // Database connection check
 db.execute('SELECT NOW()')
-  .then(() => console.log('Database connection is working'))
-  .catch((err) => console.error('Database connection check failed:', err));
+    .then(() => console.log('Database connection is working'))
+    .catch((err) => console.error('Database connection check failed:', err));
 
 // Setup Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
