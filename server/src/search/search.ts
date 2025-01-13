98a20e30a423d80dc62049d765a760f37d9bf279
InvisOn <InvisOn@users.noreply.github.com>
2025-01-14 09:52:43 +1300

    fix: search products empty pricing

--- server/src/search/search.ts
+++ server/src/search/search.ts
@@ -1,6 +1,7 @@
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
-import { ilike, asc, desc } from "drizzle-orm";
+import { ilike, asc, desc, eq } from "drizzle-orm";
 import { products } from "../db/schema/products";
+import { store_products } from "../db/schema/store_products";
 
 
 export async function search_product(db: NodePgDatabase, name: string, sort_by: 'name' | 'price' | 'amount', sort_direction: 'ASC' | 'DESC') {
@@ -11,11 +12,11 @@ export async function search_product(db: NodePgDatabase, name: string, sort_by: 
         case 'name':
             column = products.name
             break;
-        // case 'price':
-        //     column = products.price
-        //     break;
-        default:
+        case 'amount':
             column = products.amount
+            break;
+        default:
+            column = store_products.price
             break;
     }
 
@@ -23,6 +24,7 @@ export async function search_product(db: NodePgDatabase, name: string, sort_by: 
         .select()
         .from(products)
         .where(ilike(products.name, `%${name}%`))
+        .innerJoin(store_products, eq(products.id, store_products.productID))
         .orderBy(sort(column))
 
     return search_results
