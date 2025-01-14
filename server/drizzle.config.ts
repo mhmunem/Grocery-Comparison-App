c3dd4ac203e0c8bf4e381701a4c636709a62091e
InvisOn <InvisOn@users.noreply.github.com>
2025-01-15 09:58:02 +1300

    chore: refactor and remove unneeded code
    No longer need dotenv due to the fact that only a docker env will be
    used.

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,21 +1,21 @@
-    import { defineConfig } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 
-    const env = process.env.NODE_ENV || 'dev'; 
-    console.log("Running in environment:", env);
+const env = process.env.NODE_ENV || 'dev';
+console.log("Running in environment:", env);
 
-    const dbUrl = process.env[`${env.toUpperCase()}_DATABASE_URL`];
-    console.log(dbUrl);
+const dbUrl = process.env[`${env.toUpperCase()}_DATABASE_URL`];
+console.log(dbUrl);
 
-    if (!dbUrl) {
+if (!dbUrl) {
     throw new Error(`DATABASE_URL is not defined for the ${env} environment`);
-    }
+}
 
-    export default defineConfig({
-        dialect: 'postgresql',
-        schema: './src/db/schema/*',
-        dbCredentials: {
-            url: dbUrl,
-        },
-        out: "./drizzle",
-    })
+export default defineConfig({
+    dialect: 'postgresql',
+    schema: './src/db/schema/*',
+    dbCredentials: {
+        url: dbUrl,
+    },
+    out: "./drizzle",
+})
