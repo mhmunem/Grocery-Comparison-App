45a54388801bd1e1bf2c4ca5e56626b91fc37268
InvisOn <InvisOn@users.noreply.github.com>
2025-01-14 13:25:11 +1300

    fix: numeric for amount

--- server/src/db/schema/products.ts
+++ server/src/db/schema/products.ts
@@ -1,13 +1,14 @@
 import { serial, integer, text, pgTable } from "drizzle-orm/pg-core"
 import { units } from "./units"
 import { category } from "./category"
+import { numericCasted } from "../../types/types"
 
 export const products = pgTable('products', {
     id: serial().primaryKey(),
     name: text().notNull(),
     brand: text().notNull(),
     details: text().notNull(),
-    amount: integer().notNull(),
+    amount: numericCasted().notNull(),
     image: text().notNull(),
     unitID: integer().notNull().references(() => units.id),
     categoryID: integer().notNull().references(() => category.id),
