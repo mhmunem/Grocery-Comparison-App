7f0831ba84d36272111e3142b386dd1d9b9a8f26
mhmunem <munem.mh@gmail.com>
2025-01-17 10:27:54 +1300

    fix: fixed drizzle studio

---
+++ server/drizzle-dev.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "drizzle-kit";
+
+
+const env = process.env.NODE_ENV!
+// const dbUrl = process.env[`${env.toUpperCase()}_DATABASE_URL`];
+const dbUrl ="postgresql://postgres:cosc680@localhost:5432/cosc680_dev_db"
+
+if (!dbUrl) {
+    throw new Error(`DATABASE_URL is not defined for the ${env} environment`);
+}
+
+export default defineConfig({
+    dialect: 'postgresql',
+    schema: './src/db/schema/*',
+    dbCredentials: {
+        url: dbUrl,
+    },
+    out: "./src/db/drizzle",
+})
