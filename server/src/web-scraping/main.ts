2c1da0b24fe31efcbb4064b1ba5c081ea2693451
Nandhini Sambasivam <nandhini.sambasivam@pg.canterbury.ac.nz>
2025-01-27 11:00:38 +1300

    chore: grocer scraping chains, store, prodcuts and price

--- server/src/web-scraping/main.ts
+++ server/src/web-scraping/main.ts
@@ -1,25 +1,45 @@
-import chainScraper from './scraper/chainScraper'
+import GrosaveChainScraper from './scraper/GroSaveChainScraper'
+import GrocerChainScraper from './scraper/GrocerChainScraper'
+import GrocerProductsScraper from './scraper/GrocerScraper'
+import GrocerStoreScraper from './scraper/GrocerStoreScraper'
 import GroSaveCategoryScraper from './scraper/GroSaveCategoryScraper'
 import GroSaveProductsScraper from './scraper/GroSaveProductsScraper'
 import GrosaveStoresScraper from './scraper/GrosaveStoresScraper'
 
 async function runScrapers(): Promise<undefined> {
-    try {
-      await chainScraper
-      console.log("Chain scraping completed")
+  try {
 
-      await GrosaveStoresScraper
-      console.log("Stores scraped completely")
+    /* Grocer Scraper call*/
+    // Uncommented ths if we need Grocer Data and comment the grosave 
 
-      await GroSaveCategoryScraper
-      console.log('Categories scraped completely')
+    /*await GrocerChainScraper()
+    console.log("Chain scraping completed")
 
-      await GroSaveProductsScraper
-      console.log('Products scraped completely')
+    await GrocerStoreScraper
+    console.log("Store scraping completed")
 
-    } catch (error) {
-      console.error('Error during scraping:', error)
-    }
+    await GrocerProductsScraper()
+    console.log('Products scraped completed')*/
+
+
+
+    /* GroSave Scraper call*/
+
+    await GrosaveChainScraper()
+    console.log("Chain scraping completed")
+
+    await GrosaveStoresScraper()
+    console.log("Stores scraped completed")
+
+    await GroSaveCategoryScraper()
+    console.log('Categories scraped completed')
+
+    await GroSaveProductsScraper()
+    console.log('Products scraped completed')
+
+  } catch (error) {
+    console.error('Error during scraping:', error)
   }
+}
 
-  runScrapers()
+runScrapers()
---
+++ server/src/web-scraping/scraper/GrocerStoreScraper.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import insertOrUpdateStores from '../drizzleQuery/insertStores'
+
+
+async function fetchStores() {
+    const response = await axios.get('https://backend.grocer.nz/stores')
+    return response.data
+}
+
+async function storeScraper() {
+    try {
+        const storesData = await fetchStores()
+        await insertOrUpdateStores(storesData)
+        console.log('All stores inserted successfully.')
+    } catch (error) {
+        console.error('Error during scraping:', error)
+    }
+}
+
+export default storeScraper()
