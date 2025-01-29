b6184b62eb1b977f61ee94c5893e51935c6996f6
InvisOn <InvisOn@users.noreply.github.com>
2025-01-30 09:40:50 +1300

    chore: remove extranious console.log

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,24 +1,21 @@
+import About from "../pages/About/About"
+import Account from "../pages/AccountPage/AccountPage";
+import Search from "../pages/SearchPage/SearchPage";
+import ShoppingList from "../pages/ShoppingListPage/ShoppingListPage";
+import Store from "../pages/StorePage/StorePage"
 import { IonRouterOutlet } from "@ionic/react";
 import { Redirect, Route } from "react-router";
 
-// import Home from "../pages/HomePage/HomePage";
-import Search from "../pages/SearchPage/SearchPage";
-import ShoppingList from "../pages/ShoppingListPage/ShoppingListPage";
-import Account from "../pages/AccountPage/AccountPage";
-import Store from "../pages/StorePage/StorePage"
-import About from "../pages/About/About"
-
 
 const AppRoutes: React.FC = () => (
-  <IonRouterOutlet>
-    <Redirect exact path="/" to="/about" />
-    <Route path="/stores" render={() => <Store />} exact={true} />
-    {/* <Route path="/home" render={() => <Home />} exact={true} /> */}
-    <Route path="/search" render={() => <Search />} exact={true} />
-    <Route path="/shoppinglist" render={() => <ShoppingList />} exact={true} />
-    <Route path="/account" render={() => <Account />} exact={true} />
-    <Route path="/about" render={() => <About />} exact={true} />
-  </IonRouterOutlet>
+    <IonRouterOutlet>
+        <Redirect exact path="/" to="/about" />
+        <Route path="/stores" render={() => <Store />} exact={true} />
+        <Route path="/search" render={() => <Search />} exact={true} />
+        <Route path="/shoppinglist" render={() => <ShoppingList />} exact={true} />
+        <Route path="/account" render={() => <Account />} exact={true} />
+        <Route path="/about" render={() => <About />} exact={true} />
+    </IonRouterOutlet>
 );
 
 export default AppRoutes;
