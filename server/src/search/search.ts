b10c1cc798759f4157bbba9ed56a6095f128698a
Nandhini Sambasivam <nandhini.sambasivam@pg.canterbury.ac.nz>
2025-02-02 10:50:45 +1300

    chore: limited the stores to achieve optimization quicker

--- server/src/search/search.ts
+++ server/src/search/search.ts
@@ -1,5 +1,5 @@
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
-import { ilike, asc, desc, eq, gte, and, ne } from "drizzle-orm";
+import { ilike, asc, desc, eq, gte, and, ne, inArray } from "drizzle-orm";
 import { products } from "../db/schema/products";
 import { store_products } from "../db/schema/store_products";
 import { units } from "../db/schema/units";
@@ -11,6 +11,12 @@ import { ProductSearchResult } from "../types/schema";
 export async function search_product(db: NodePgDatabase, name: string, sort_by: 'name' | 'price' | 'amount', sort_direction: 'ASC' | 'DESC'): Promise<ProductSearchResult[]> {
     const sort = sort_direction == 'ASC' ? asc : desc
     let column
+    const storeIds = [2, 5, 6, 10, 15, 28, 30, 32, 38, 41,
+        58, 64, 73, 90, 99, 104, 129, 134, 141, 158,
+        162, 163, 173, 186, 192, 195, 197, 207, 208, 211,
+        212, 217, 221, 234, 246, 249, 266, 267, 275, 283,
+        286, 288, 296, 333, 336, 339, 344, 357, 361, 367,
+        384, 398, 403, 409, 417, 422, 423, 434];
 
     switch (sort_by) {
         case 'name':
@@ -27,7 +33,7 @@ export async function search_product(db: NodePgDatabase, name: string, sort_by: 
     const search_results = await db
         .select()
         .from(products)
-        .where(ilike(products.name, `%${name}%`))
+        .where(and(ilike(products.name, `%${name}%`), inArray(store_products.storeID, storeIds)))
         .innerJoin(store_products,  and(eq(products.id, store_products.productID), ne(store_products.price, 0)))
         .innerJoin(units, eq(products.unitID, units.id))
         .innerJoin(category, eq(products.categoryID, category.id))
